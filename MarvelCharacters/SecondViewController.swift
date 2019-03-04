d8a481f7840703504d70f57f9e2ba4ce8d62b775
Jaz King <jazking@jazs-macbook-pro.local>
2019-03-04 15:36:56 +0000

    Initial Commit

---
+++ MarvelCharacters/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MarvelCharacters
+//
+//  Created by Jaz King on 04/03/2019.
+//  Copyright © 2019 Jaz King. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
